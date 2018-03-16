62495316451d8363e09bde1f5170bedb1bd73ad4
godinlove <ttttttliu@qq.com>
2018-03-16 11:22:55 +0800

    add setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+self_version = "0.0"
+
+try:
+    LONG_DESCRIPTION = open("README.rst", "rb").read().decode("utf-8")
+except IOError:
+    LONG_DESCRIPTION = "an independent demmo of KNOWN operon predict method"
 
 setup(
     name='operondemmo',
-    version='',
-    packages=['operondemmo'],
-    url='',
-    license='',
+    version=self_version,
+    packages=find_packages(),
+    url='https://github.com/GodInLove/operondemmo',
+    license='GPL-3.0',
     author='yaodongliu',
     author_email='yd.liu.scu@gmail.com',
-    description=''
+    description=LONG_DESCRIPTION,
 )
