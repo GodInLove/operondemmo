40c692a7970a50be2fbfede658b8ff4a7e42e769
godinlove <ttttttliu@qq.com>
2018-03-16 11:05:09 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='operondemmo',
+    version='',
+    packages=['operondemmo'],
+    url='',
+    license='',
+    author='yaodongliu',
+    author_email='yd.liu.scu@gmail.com',
+    description=''
+)
