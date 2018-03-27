3c0c3f6a9e7529629e803e3b1734e05f7c4bccc4
godinlove <ttttttliu@qq.com>
2018-03-27 16:37:50 +0800

    modify roc function

--- roc_curve/compute_tp_fp.py
+++ roc_curve/compute_tp_fp.py
@@ -6,7 +6,8 @@ from operondemmo.hierarchical_cluster.gamma_domain import get_result_by_clusteri
 
 from operondemmo.input_file_handle.handle_gff import get_gene_pos_strand, sorted_gene, \
     from_simple_gff_information_to_get
-from operondemmo.operon import from_depth_file_to_get_co_matrix_co_expression, load_from_input_files
+from operondemmo.operon import from_depth_file_to_get_co_matrix_co_expression, \
+    from_fastq_file_to_get_co_matrix_co_expression
 
 
 def compute_n(list_sort, p_or_n_predict):
@@ -27,7 +28,7 @@ def compute_n(list_sort, p_or_n_predict):
 def generate_output_files(simple_gff_file, depth_files, co_method, out_path, static_threshold_list):
     _gene_pos, _gene_strand = get_gene_pos_strand(simple_gff_file)
     gene_strand_m, gene_index_m, sorted_gene_m = from_simple_gff_information_to_get(_gene_pos, _gene_strand)
-    matrix_co = from_depth_file_to_get_co_matrix_co_expression(depth_files, _gene_pos, co_method)
+    matrix_co = from_depth_file_to_get_co_matrix_co_expression(depth_files, _gene_pos, co_method, 1)
     i_iter = 0
     for each_t in static_threshold_list:
         if i_iter < 10:
@@ -53,8 +54,8 @@ def get_list_from_file(file_path):
     return f_p_out_sort
 
 
-def roc_curve(simple_gff, depth_files, method, out_path, p_d_list, n_d_list, tp_fp_file, roc_file_path, threshold_list):
-    generate_output_files(simple_gff, depth_files, method, out_path, threshold_list)
+def roc_curve(simple_gff, depth_files, method, out_path, p_d_list, n_d_list, tp_fp_file, roc_file_path, t_list):
+    generate_output_files(simple_gff, depth_files, method, out_path, t_list)
     list_files = os.listdir(out_path)
     list_files = sorted(list_files)
     tp_fp_file_fp = open(tp_fp_file, 'w')
@@ -69,18 +70,37 @@ def roc_curve(simple_gff, depth_files, method, out_path, p_d_list, n_d_list, tp_
         print(tp, fp)
         tpr = tp / p
         fpr = fp / n
-        print(tpr, fpr)
-        tpr_fpr_file_fp.write(str(tpr) + "\t" + str(fpr) + "\n")
+        pre = tp / (tp + fp)
+        # print(tpr, fpr, pre)
+        tpr_fpr_file_fp.write(str(tpr) + "\t" + str(fpr) + "\t" + str(pre) + "\n")
     tp_fp_file_fp.close()
     tpr_fpr_file_fp.close()
     matrix_a = numpy.loadtxt(roc_file_path)
     tpr = matrix_a[..., 0].tolist()
     fpr = matrix_a[..., 1].tolist()
-    return tpr, fpr
+    pre = matrix_a[..., 2].tolist()
+    return tpr, fpr, pre
 
 
-def ss_curve(simple_gff, depth_files, method, out_path, p_d_list, n_d_list, tp_fp_file, roc_file_path, threshold_list):
-    generate_output_files(simple_gff, depth_files, method, out_path, threshold_list)
+def generate_kallisto_output_files(simple_gff, input_files, method, out_path, t_list):
+    _gene_pos, _gene_strand = get_gene_pos_strand(simple_gff)
+    gene_strand_m, gene_index_m, sorted_gene_m = from_simple_gff_information_to_get(_gene_pos, _gene_strand)
+    matrix_co = from_fastq_file_to_get_co_matrix_co_expression(input_files, out_path + "tmp/", _gene_pos, method, 1)
+    i_iter = 0
+    for each_t in t_list:
+        if i_iter < 10:
+            tmp_name = "0" + str(i_iter)
+        else:
+            tmp_name = str(i_iter)
+        result_file = out_path + tmp_name + ".out"
+        get_result_by_clustering2(result_file, gene_strand_m, gene_index_m, sorted_gene_m, matrix_co, each_t)
+        i_iter = i_iter + 1
+
+
+def kallisto_roc_curve(simple_gff, depth_files, method, out_path, p_d_list, n_d_list, tp_fp_file, roc_file_path,
+                       t_list):
+    generate_kallisto_output_files(simple_gff, depth_files, method, out_path, t_list)
+    os.system("rm -r -f" + out_path + "tmp/")
     list_files = os.listdir(out_path)
     list_files = sorted(list_files)
     tp_fp_file_fp = open(tp_fp_file, 'w')
@@ -94,15 +114,17 @@ def ss_curve(simple_gff, depth_files, method, out_path, p_d_list, n_d_list, tp_f
         tp_fp_file_fp.write(str(tp) + "\t" + str(fp) + "\n")
         print(tp, fp)
         tpr = tp / p
-        spc = 1 - fp / n
-        print(tpr, spc)
-        tpr_fpr_file_fp.write(str(tpr) + "\t" + str(spc) + "\n")
+        fpr = fp / n
+        pre = tp / (tp + fp)
+        # print(tpr, fpr, pre)
+        tpr_fpr_file_fp.write(str(tpr) + "\t" + str(fpr) + "\t" + str(pre) + "\n")
     tp_fp_file_fp.close()
     tpr_fpr_file_fp.close()
     matrix_a = numpy.loadtxt(roc_file_path)
     tpr = matrix_a[..., 0].tolist()
-    spc = matrix_a[..., 1].tolist()
-    return tpr, spc
+    fpr = matrix_a[..., 1].tolist()
+    pre = matrix_a[..., 2].tolist()
+    return tpr, fpr, pre
 
 
 def get_t_list(start, stop, n):
@@ -118,42 +140,80 @@ def get_t_list(start, stop, n):
 if __name__ == "__main__":
     path = "/home/lyd/document/2018.1/gamma_domain/"
     eco_simple_gff = path + "simple_eco.gff_3"
-    eco_depth_files = load_from_input_files(path + "eco_count/")
-    tmp_out_path = path + "g_d_out/"
-    co_expression_method = 0
+    eco_depth_files = path + "eco_count/"
+    eco_input_files = path + "eco_fastqs/"
     roc_path = "/home/lyd/document/2018.1/roc/"
     positive_data = roc_path + "positiveData.txt"
     negative_data = roc_path + "negativeData.txt"
+
+    tmp_out_path = path + "g_d_out/"
+    co_expression_method = 0
     tmp_tp_fp_file = path + "tp_fp.txt"
     tmp_roc_file_path = path + "tpr_fpr.txt"
-    threshold_list = get_t_list(-1, 1, 50)
+    threshold_list = get_t_list(-1, 1, 100)
     p_d_sort_list = get_list_from_file(positive_data)
     n_d_sort_list = get_list_from_file(negative_data)
-    tpr_0, fpr_0 = roc_curve(eco_simple_gff, eco_depth_files, co_expression_method, tmp_out_path,
-                             p_d_sort_list, n_d_sort_list, tmp_tp_fp_file, tmp_roc_file_path, threshold_list)
-
+    tpr_0, fpr_0, pre_0 = roc_curve(eco_simple_gff, eco_depth_files, co_expression_method, tmp_out_path,
+                                    p_d_sort_list, n_d_sort_list, tmp_tp_fp_file, tmp_roc_file_path, threshold_list)
+    tmp_out_path = path + "g_d_out_00/"
+    tmp_tp_fp_file = path + "tp_fp_00.txt"
+    tmp_roc_file_path = path + "tpr_fpr_00.txt"
+    tpr_00, fpr_00, pre_00 = kallisto_roc_curve(eco_simple_gff, eco_input_files, co_expression_method, tmp_out_path,
+                                                p_d_sort_list, n_d_sort_list, tmp_tp_fp_file, tmp_roc_file_path,
+                                                threshold_list)
     tmp_out_path = path + "g_d_out_1/"
     co_expression_method = 1
     tmp_tp_fp_file = path + "tp_fp_1.txt"
     tmp_roc_file_path = path + "tpr_fpr_1.txt"
-    tpr_1, fpr_1 = roc_curve(eco_simple_gff, eco_depth_files, co_expression_method, tmp_out_path,
-                             p_d_sort_list, n_d_sort_list, tmp_tp_fp_file, tmp_roc_file_path, threshold_list)
-
+    tpr_1, fpr_1, pre_1 = roc_curve(eco_simple_gff, eco_depth_files, co_expression_method, tmp_out_path,
+                                    p_d_sort_list, n_d_sort_list, tmp_tp_fp_file, tmp_roc_file_path, threshold_list)
+    tmp_out_path = path + "g_d_out_11/"
+    tmp_tp_fp_file = path + "tp_fp_11.txt"
+    tmp_roc_file_path = path + "tpr_fpr_11.txt"
+    tpr_11, fpr_11, pre_11 = kallisto_roc_curve(eco_simple_gff, eco_input_files, co_expression_method, tmp_out_path,
+                                                p_d_sort_list, n_d_sort_list, tmp_tp_fp_file, tmp_roc_file_path,
+                                                threshold_list)
     tmp_out_path = path + "g_d_out_2/"
     co_expression_method = 2
     tmp_tp_fp_file = path + "tp_fp_2.txt"
     tmp_roc_file_path = path + "tpr_fpr_2.txt"
-    tpr_2, fpr_2 = roc_curve(eco_simple_gff, eco_depth_files, co_expression_method, tmp_out_path,
-                             p_d_sort_list, n_d_sort_list, tmp_tp_fp_file, tmp_roc_file_path, threshold_list)
+    tpr_2, fpr_2, pre_2 = roc_curve(eco_simple_gff, eco_depth_files, co_expression_method, tmp_out_path,
+                                    p_d_sort_list, n_d_sort_list, tmp_tp_fp_file, tmp_roc_file_path, threshold_list)
+    tmp_out_path = path + "g_d_out_22/"
+    tmp_tp_fp_file = path + "tp_fp_22.txt"
+    tmp_roc_file_path = path + "tpr_fpr_22.txt"
+    tpr_22, fpr_22, pre_22 = kallisto_roc_curve(eco_simple_gff, eco_input_files, co_expression_method, tmp_out_path,
+                                                p_d_sort_list, n_d_sort_list, tmp_tp_fp_file, tmp_roc_file_path,
+                                                threshold_list)
+    plt.figure(dpi=1000)
     plt.style.use('ggplot')
-    plt.title("ROC curve of gamma_domain algorithm\n(AUC="")")
+    plt.title("ROC curve of gamma_domain algorithm")
     plt.xlabel("False Positive Rate")
     plt.ylabel("True Positive Rate")
-    plt.xlim(0)
-    plt.ylim(0)
-    plt.plot(fpr_0, tpr_0, label="c_i_j")
-    plt.plot(fpr_1, tpr_1, label="person")
-    plt.plot(fpr_2, tpr_2, label="spearman")
+    plt.xlim(0, 0.03)
+    plt.ylim(0.18, 0.7)
+    plt.plot(fpr_0, tpr_0, label="c_i_j", linewidth=0.25, color="red", linestyle="dashed")
+    plt.plot(fpr_1, tpr_1, label="person", linewidth=0.25, color="green", linestyle="dashed")
+    plt.plot(fpr_2, tpr_2, label="spearman", linewidth=0.25, color="blue", linestyle="dashed")
+    plt.plot(fpr_00, tpr_00, label="c_i_j_k", linewidth=0.25, color="red", linestyle="solid")
+    plt.plot(fpr_11, tpr_11, label="person_k", linewidth=0.25, color="green", linestyle="solid")
+    plt.plot(fpr_22, tpr_22, label="spearman_k", linewidth=0.25, color="blue", linestyle="solid")
     plt.plot([0, 1], [0, 1], '--', color="black", label='Luck')
-    plt.legend()
-    plt.show()
+    plt.legend(loc=4)
+    plt.savefig("roc_curve.svg", format="svg")
+
+    plt.figure(dpi=1000)
+    plt.style.use('ggplot')
+    plt.title("precision curve of gamma_domain algorithm")
+    plt.xlabel("Recall")
+    plt.ylabel("Precision")
+    plt.xlim(0.18, 0.7)
+    plt.ylim(0.4, 0.8)
+    plt.plot(tpr_0, pre_0, label="c_i_j", linewidth=0.25, color="red", linestyle="dashed")
+    plt.plot(tpr_1, pre_1, label="person", linewidth=0.25, color="green", linestyle="dashed")
+    plt.plot(tpr_2, pre_2, label="spearman", linewidth=0.25, color="blue", linestyle="dashed")
+    plt.plot(tpr_00, pre_00, label="c_i_j_k", linewidth=0.25, color="red", linestyle="solid")
+    plt.plot(tpr_11, pre_11, label="person_k", linewidth=0.25, color="green", linestyle="solid")
+    plt.plot(tpr_22, pre_22, label="spearman_k", linewidth=0.25, color="blue", linestyle="solid")
+    plt.legend(loc=1)
+    plt.savefig("pre_rec.svg", format="svg")
